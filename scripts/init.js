f40b999da80e17f6f0064bfd4133f9a34359c31f
mark pearce <mark.pearce72@gmail.com>
2022-05-27 11:51:14 +0200

    fix namespace to -fork

--- scripts/init.js
+++ scripts/init.js
@@ -3,7 +3,7 @@ import { skillChallenge } from './scripts.js'
 // Set up socket listener to listen for gm_skillset macro
 Hooks.once('ready', () => {
   console.log('PF2e RSC | hooked in')
-  game.socket.on('module.pf2e-rsc', (data) => {
+  game.socket.on('module.pf2e-fork', (data) => {
     if (data.operation === 'playerSkillChallenge') {
       if (data.actor.permission[game.user.id] >= 3) {
         skillChallenge(
