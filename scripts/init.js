10bd6b04ff65f95b1eae3aa8c4ba5e81eb8bbc53
mark pearce <mark.pearce72@gmail.com>
2022-05-27 12:34:08 +0200

    fix socket

--- scripts/init.js
+++ scripts/init.js
@@ -3,8 +3,13 @@ import { skillChallenge } from './scripts.js'
 // Set up socket listener to listen for gm_skillset macro
 Hooks.once('ready', () => {
   console.log('PF2e RSC | hooked in')
-  game.socket.on('module.pf2e-fork', (data) => {
+  game.socket.on('module.pf2e-rsc-fork', (data) => {
+    console.log('rsc-fork')
     if (data.operation === 'playerSkillChallenge') {
+      console.log(
+        'challenge recieved. permisision:' +
+          data.actor.permission[game.user.id],
+      )
       if (data.actor.permission[game.user.id] >= 3) {
         skillChallenge(
           data.neededSuccesses,
